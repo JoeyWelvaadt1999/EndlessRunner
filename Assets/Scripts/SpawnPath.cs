98ddca822f5a58622b8410c8ffdc7089e8bf3ab6
SuperSaiyan4 <joeywelvaadt@gmail.com>
2015-08-31 13:21:42 +0200

    31-07-2015 13:21

--- Assets/Scripts/SpawnPath.cs
+++ Assets/Scripts/SpawnPath.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class SpawnPath : MonoBehaviour {
+	public GameObject blockPrefab;
+	private int totalSpawnedBlocks;
+	private Vector3 screenSize;
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log(Screen.width);
+		screenSize = Camera.main.ViewportToWorldPoint(new Vector3(1.0f, 1.0f, 0.0f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		SpawnBlocks();
+	}
+
+	void SpawnBlocks() {
+		Instantiate(blockPrefab, new Vector3((screenSize.x * -1) + (1*totalSpawnedBlocks),(screenSize.x * -0.45f) ,0), Quaternion.identity);
+		totalSpawnedBlocks++;
 	}
 }
